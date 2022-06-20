2fd99454eb60a41df654fa5f2060525108c3088a
EduGB-max <84384908+EduGB-max@users.noreply.github.com>
2022-06-20 10:05:48 -0300

    Add files via upload

---
+++ ac1.js
@@ -0,0 +1,30 @@
+
+
+var restaurante={
+Lanches: [Um,Dois,Três],
+Acompanhamentos:[batatas,'Bebida', 'Fruta'],
+}
+
+
+var Um = {
+    nome: 'Mc Lanche Alegre',
+    Ingredientes : ['Carne', 'Queijo'],
+    Acompanhamentos : ['Fritas', 'Fruta']
+}
+
+var Dois = {
+    nome: 'Big Lanche',
+    Ingredientes : ['Dois hamburgueres', 'Alface', 'Queijo', 'Molho Bacana', 'Cebola', 'Picles'],
+    Acompanhamentos : ['Fritas', 'Bebida']
+}
+
+var Três = {
+    nome: 'Mac Hem',
+    Ingredientes : ['Frango', 'Alface', 'Queijo', 'Picles'],
+    Acompanhamentos : [batatas, 'Bebida']
+}
+
+var batatas ={
+tipo: 'fritas',
+unidade: ['pequena', 'média']
+}
